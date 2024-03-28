e06953a156843f46edc443ae3dd903c33ce9c247
Samaan Kamal <samaankamal72@gmail.com>
2024-03-28 02:59:10 +0200

    finished products

--- src/main/java/com/example/SpringBootTask/Products/Repository/ProductRepository.java
+++ src/main/java/com/example/SpringBootTask/Products/Repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.example.SpringBootTask.Products.Repository;
 import com.example.SpringBootTask.Products.Entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
